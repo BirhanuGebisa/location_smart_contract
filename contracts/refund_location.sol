bfee5bbfce2e9f732a74a99c420ea7f6cfc97761
BirhanuGebisa <birhanugebisa@gmail.com>
2022-10-29 18:03:57 +0300

    solidity update

--- contracts/refund_location.sol
+++ contracts/refund_location.sol
@@ -1,10 +1,9 @@
-//SPDX-License-Identifier: UNLICENSED
+//SPDX-License-Identifier: MIT
 
 // Solidity files have to start with this pragma.
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
-import "hardhat/console.sol";
 // This is the main building block for smart contracts.
 contract Employe {
     string[2] location = ["0.00", "0.00"];
