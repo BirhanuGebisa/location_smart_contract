100f577dbb74a5f0952e6496f5d4390d03d97d82
BirhanuGebisa <birhanugebisa@gmail.com>
2022-10-29 18:02:46 +0300

    truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -80,6 +80,14 @@ module.exports = {
     //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
     // },
     //
+    goerli: {
+      provider: () => {
+        return new HDWalletProvider(process.env.MNEMONIC, 'https://goerli.infura.io/v3/' + process.env.INFURA_API_KEY)
+      },
+      network_id: '5', // eslint-disable-line camelcase
+      gas: 4465030,
+      gasPrice: 10000000000,
+    },
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
     // goerli: {
