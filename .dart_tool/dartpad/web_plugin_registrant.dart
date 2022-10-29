6c9995140572658d46cd5ae34c31b4de32ac50bb
BirhanuGebisa <birhanugebisa@gmail.com>
2022-10-29 22:59:54 +0300

    web contracts

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,6 +6,7 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
 import 'package:location_web/location_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -13,6 +14,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FlutterNativeSplashWeb.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
   LocationWebPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
